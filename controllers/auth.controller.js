755175be64bd201d3b8dd748da720376a676e4a6
geunseonkim <guensoen@gmail.com>
2024-06-14 02:25:54 +1200

    create google login system

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,10 @@
 const User = require("../model/User");
+const { OAuth2Client } = require("google-auth-library");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 const authController = {};
 
@@ -18,6 +20,37 @@ authController.loginWithEmail = async (req, res) => {
       }
     }
     throw new Error("invalid email or password");
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err.message });
+  }
+};
+
+authController.loginWithGoogle = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+    const { email, name } = ticket.getPayload();
+    console.log("google-email, name ", email, name);
+    // google auth library: node.js client
+    // 4. 백엔드에서 로그인 하기.
+    //   a. 이미 로그인을 한 적이 있는 유저 -> 로그인 시키고, 토큰값 주기.
+    //   b. 처음 로그인을 시도한 유저 -> 유저 정보를 새로 생성하고, 토큰값 주기.
+    let user = await User.findOne({ email });
+    if (!user) {
+      // 유저를 새로 생성.
+      const randomPassword = "" + Math.floor(Math.random() * 100000000);
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(randomPassword, salt);
+      user = new User({ name: name, email: email, password: newPassword });
+      await user.save();
+    }
+    // 토큰 발행 리턴.
+    const sessionToken = await user.generateToken();
+    res.status(200).json({ status: "success", user, token: sessionToken });
   } catch (err) {
     res.status(400).json({ status: "fail", error: err.message });
   }
--- routes/product.api.js
+++ routes/product.api.js
@@ -1,13 +1,28 @@
-const express = require("express")
-const router = express.Router()
-const authController = require("../controllers/auth.controller")
-const productController = require("../controllers/product.controller")
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth.controller");
+const productController = require("../controllers/product.controller");
 
-router.post("/", authController.authenticate, authController.checkAdminPermission, productController.createProduct)
+router.post(
+  "/",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.createProduct
+);
 // 상품은 누구나 다 만들 수 없다!
-router.get("/", productController.getProducts)
-router.get("/:id", productController.getProductById)
-router.put("/:id", authController.authenticate, authController.checkAdminPermission, productController.updateProduct)
-router.delete("/:id",authController.authenticate, authController.checkAdminPermission, productController.deleteProduct)
+router.get("/", productController.getProducts);
+router.get("/:id", productController.getProductById);
+router.put(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.updateProduct
+);
+router.delete(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.deleteProduct
+);
 
-module.exports = router
+module.exports = router;
